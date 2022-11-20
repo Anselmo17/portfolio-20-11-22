f62c5b67b9dd215875a9ebe1c376c38047e081be
Anselmo <ansinftec17@gmail.com>
2022-11-20 11:30:45 -0300

    Criando estrutura inicial do projeto portfolio

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Portfólio
+- Projeto criado para mostrar minha evolução profissional como desenvolvedor de sistemas.
+- Onde estarei colocando meus contatos , experiência e skills no qual sou especializado.
---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+// lib carousel
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
