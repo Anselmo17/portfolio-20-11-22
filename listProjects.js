efc3baff18a8652cb96a11261b3f9c72bde3b877
Anselmo <ansinftec17@gmail.com>
2022-12-03 11:50:02 -0300

    FEAT:Adicionando informações na seção de projetos

---
+++ listProjects.js
@@ -0,0 +1,39 @@
+
+projects = [
+    {
+        image: './images/yu-yu-hakusho.png',
+        title: 'Quiz Yu-Yu-Hakusho',
+        message: 'Nessa aplicação foi construído um quiz com o anime Yu-Yu-Hakusho.',
+        link: 'https://quiz-yu-yu-hakusho.vercel.app/'
+    },
+    {
+        image: './images/sistema-produto.png',
+        title: 'Sistema de Cadastro de Produtos',
+        message: 'Nessa aplicação foi construído uma aplicação para cadastro de produtos.',
+        link: 'https://sistema-de-produtos.vercel.app/'
+    },
+    {
+        image: './images/pokemons.png',
+        title: 'Lista de pokemóns',
+        message: 'Nesta aplicação foi criado uma lista de cards pokemóns onde podempos ver qual o tipo de pokemóns.',
+        link: 'https://cards-pokemon.vercel.app/'
+    },
+    {
+        image: './images/clima.png',
+        title: 'Clima de Cidades',
+        message: 'Nesta aplicação foi criado uma pesquisa de clima por cidades.',
+        link: 'https://clima-cidade-h0eiltqid-anselmo17.vercel.app/'
+    },
+    {
+        image: './images/github.png',
+        title: 'Pesquisa perfil GitHub',
+        message: 'Nesta aplicação foi criado uma pesquisa por perfil no repositório no gitHub.',
+        link: 'https://perfis-gi-hub.vercel.app/'
+    },
+    {
+        image: './images/calcImc.png',
+        title: 'Calculo IMC',
+        message: 'Nesta aplicação criamos um calculo IMC corporal onde você consegui ver o nível de gordura corporal que você se encontra.',
+        link: 'https://calculo-imc-iota.vercel.app/'
+    }
+];
